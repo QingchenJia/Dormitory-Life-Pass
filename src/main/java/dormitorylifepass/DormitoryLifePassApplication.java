a71a99d557fd091109d0a5b5d4e10dc2bde0d160
Qingchen_Jia <879484952@qq.com>
2024-12-14 15:29:42 +0800

    Configure the basic information of the API document and print the log at startup

--- src/main/java/dormitorylifepass/DormitoryLifePassApplication.java
+++ src/main/java/dormitorylifepass/DormitoryLifePassApplication.java
@@ -1,13 +1,22 @@
 package dormitorylifepass;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class DormitoryLifePassApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DormitoryLifePassApplication.class, args);
+        log.info("""
+                                  
+                -------------------------------------------------------
+                \tApplication:\tDormitoryLifePass Starting...
+                \tApi-doc:\thttp://localhost:8080/doc.html
+                -------------------------------------------------------
+                """);
     }
 
 }
